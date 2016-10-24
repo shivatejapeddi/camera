fa2747c3a2f87a21b4cb57740de9427fed76b46c
Mansoor Aftab <maftab@codeaurora.org>
2016-10-23 19:31:15 -0700

    QCamera2: HAL3: Enable HDR snapshot support
    Add orchestrator method and add logic to orchestrate
    a HDR snapshot request
    
    Change-Id: Ia9f51e37d4ee9668fb9ad184861a69f060fa02be

--- QCamera2/stack/common/cam_types.h
+++ QCamera2/stack/common/cam_types.h
@@ -107,7 +107,7 @@
 #define EXIF_IMAGE_DESCRIPTION_SIZE 100
 
 #define MAX_INFLIGHT_REQUESTS  6
-#define MAX_INFLIGHT_BLOB      3
+#define MAX_INFLIGHT_BLOB      10
 #define MIN_INFLIGHT_REQUESTS  3
 #define MIN_INFLIGHT_60FPS_REQUESTS (6)
 #define MAX_INFLIGHT_REPROCESS_REQUESTS 1
@@ -2201,6 +2201,8 @@ typedef enum {
     /* Param of scaling information for JPEG encoder */
     CAM_INTF_PARM_JPEG_SCALE_DIMENSION,
     CAM_INTF_META_FOCUS_DEPTH_INFO,
+    /* HAL based HDR*/
+    CAM_INTF_PARM_HAL_BRACKETING_HDR,
     CAM_INTF_PARM_MAX
 } cam_intf_parm_type_t;
 
